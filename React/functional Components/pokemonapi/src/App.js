7599794ac24b0d68ed107b9cc1bdf663c9db1d29
LanaTheodory <81506605+LanaTheodory@users.noreply.github.com>
2021-07-25 14:33:08 +0300

    pokemon api

---
+++ React/functional Components/pokemonapi/src/App.js
@@ -0,0 +1,45 @@
+
+import './App.css';
+import { useEffect , useState} from 'react';
+
+function App() {
+ 
+    const [names, setNames] = useState([]);
+    
+    // const showNames = () =>{
+    //   return(
+        
+    //     names.length > 0 ? names.map((names, index)=>{
+    //         return (<div key={index}>{names.name}</div>)
+    //     }) : "loading"
+ 
+    //   )
+    // }
+ 
+    const showNames = () => {
+        fetch("https://pokeapi.co/api/v2/pokemon?offset=807&limit=311")
+            .then(response => response.json())
+            .then(response => setNames(response.results))
+             
+        
+       
+    };
+ 
+    return (
+      <>
+      <button onClick={showNames }>Show Names</button>
+      {names.length > 0 ? names.map((names, index)=>{
+            return (<div key={index}>{names.name}</div>)
+        }) : ""}
+ 
+      )
+   
+      </>
+    );
+
+
+
+  
+}
+
+export default App;
