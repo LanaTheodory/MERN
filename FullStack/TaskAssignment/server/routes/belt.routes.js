ab055fc17caefc4c4b9d2139e0feff8ac744b957
LanaTheodory <81506605+LanaTheodory@users.noreply.github.com>
2021-08-08 03:19:13 +0300

    Delete FullStack/TaskAssignment directory

--- FullStack/TaskAssignment/server/routes/belt.routes.js
+++
@@ -1,9 +0,0 @@
-const beltControllers = require("../controllers/belt.controller");
-
-module.exports = app => {
-  app.get("/api/belt", beltControllers.getAllBelts);
-  app.get("/api/belt/:id", beltControllers.getOneBelt);
-  app.post("/api/belt", beltControllers.createBelt);
-  app.put("/api/belt/:id", beltControllers.updateBelt);
-  app.delete("/api/belt/:id", beltControllers.deleteBelt);
-};
